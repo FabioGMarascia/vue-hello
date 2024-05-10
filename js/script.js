23b412a3e3c5182a85531ee601c1540b607fc92e
FabioGMarascia <fabiogaspare.marascia@gmail.com>
2024-05-10 15:14:54 +0200

    hello vue

--- js/script.js
+++ js/script.js
@@ -3,7 +3,8 @@ const { createApp } = Vue;
 createApp({
 	data() {
 		return {
-			message: "Hello Vue!",
+			message: "Hello from class #125 Vue!",
+			myStile: `text-center fw-bold text-warning mt-5 display-3`,
 		};
 	},
 }).mount("#app");
