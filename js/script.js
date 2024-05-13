260b51a3f8d5422001b506a34cd04da0a679f785
FabioGMarascia <fabiogaspare.marascia@gmail.com>
2024-05-13 10:07:58 +0200

    input for h1

--- js/script.js
+++ js/script.js
@@ -4,9 +4,10 @@ createApp({
 	data() {
 		return {
 			message: "Hello from class #125 Vue!",
-			myH1Style: `text-center fw-bold text-warning mt-5 display-3`,
+			myH1Style: `text-center fw-bold text-warning mt-4 display-3`,
 			myImg: `./img/one.png`,
 			myImgStyle: `d-block mx-auto w-50 border border-5 rounded`,
+			blue: `text-primary`,
 		};
 	},
 }).mount("#app");
