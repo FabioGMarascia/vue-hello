c3b596626cb54b935dfc8c017f54227579aafe0c
FabioGMarascia <fabiogaspare.marascia@gmail.com>
2024-05-13 11:17:39 +0200

    change img at click

--- js/script.js
+++ js/script.js
@@ -7,6 +7,18 @@ createApp({
 			myH1Style: `text-center fw-bold text-warning mt-4 display-3`,
 			myImg: `./img/one.png`,
 			myImgStyle: `d-block mx-auto w-50 border border-5 rounded`,
+			toggle: 0,
 		};
+	},
+	methods: {
+		changePath() {
+			if (this.toggle == 0) {
+				this.myImg = `./img/two.jpg`;
+				this.toggle = 1;
+			} else {
+				this.toggle = 0;
+				this.myImg = `./img/one.png`;
+			}
+		},
 	},
 }).mount("#app");
