0f6bb51822e386e0eddcf0215a5318fe06fbca82
FabioGMarascia <fabiogaspare.marascia@gmail.com>
2024-05-13 10:46:36 +0200

    useless code

--- js/script.js
+++ js/script.js
@@ -7,7 +7,6 @@ createApp({
 			myH1Style: `text-center fw-bold text-warning mt-4 display-3`,
 			myImg: `./img/one.png`,
 			myImgStyle: `d-block mx-auto w-50 border border-5 rounded`,
-			blue: `text-primary`,
 		};
 	},
 }).mount("#app");
